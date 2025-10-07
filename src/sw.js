fe6478fcaaf47fcd61d6d3ec09938c28c4a3a39e
ohio-math <fschwieb@protonmail.com>
2025-10-07 16:35:13 -0400

    Update sw.js

--- src/sw.js
+++ src/sw.js
@@ -1,9 +1,18 @@
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", event => {  
   let request = event.request;
+  let letpass = () => { event.respondWith(fetch(request.url)) }; // Makes my life easier
+  
   let config = {
-    proxyServer: "https://fschwieb-2597dad56586.herokuapp.com",
-    api: "https://fschwieb-2597dad56586.herokuapp.com/prox?url=*url*"
+    proxyServer: "https://fschwieb-2597dad56586.herokuapp.com", // Modify for other sites
+    api: "https://fschwieb-2597dad56586.herokuapp.com/prox?url=*url*" // Modify for other servers
   };
   
-  if ()
+  if (request.url.startsWith(config.proxyServer)) {
+    letpass();
+    return;
+  };
+
+  let encoded = encodeURIComponent(request.url);
+  let modifiedURL = config.api.replace(/\*url\*/, encoded);
+  event.respondWith( fetch(modifiedURL) );
 });
