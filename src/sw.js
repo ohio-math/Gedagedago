e1baa7bb260309f95e389e7b0ede7b82b2e42e0c
ohio-math <fschwieb@protonmail.com>
2025-10-07 14:55:25 -0400

    Create sw.js

---
+++ src/sw.js
@@ -0,0 +1,9 @@
+self.addEventListener("fetch", event => {
+  let request = event.request;
+  let config = {
+    proxyServer: "https://fschwieb-2597dad56586.herokuapp.com",
+    api: "https://fschwieb-2597dad56586.herokuapp.com/prox?url=*url*"
+  };
+  
+  if ()
+});
