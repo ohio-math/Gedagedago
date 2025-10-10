9aec99d343cbfb355f9413aed865cd426066b403
ohio-math <fschwieb@protonmail.com>
2025-10-10 17:17:14 -0400

    Update register.js

--- src/register.js
+++ src/register.js
@@ -1,14 +1,24 @@
-if ('serviceWorker' in navigator) {
-  messagebox.innerText = "Attempting to register service worker...";
-  navigator.serviceWorker.register("./sw.js", { scope: "/" })
-    .then(() => {
-      messagebox.innerText = "Service worker successfully registered";
-    })
-    .then((error) => {
-      messagebox.innerText = "Service worker registration failed";
-    });
-  
-} else {
-  messagebox.innerText = "Service Workers are not supported in your browser. Your navigator is: " + navigator.userAgent;
+function msg(m, box, debug) {
+  if (debug == false) return;
+  if (box == null) {
+    console.log(m);
+  } else {
+    box.innerText = m;
+  }
 }
 
+export function registerSW(messagebox = null, debug = false) {
+  if ('serviceWorker' in navigator) {
+    msg("Attempting to register service worker...", messagebox, debug);
+    navigator.serviceWorker.register("./sw.js", { scope: "/" })
+    .then(() => {
+      msg("Service worker successfully registered", messagebox, debug);
+    })
+    .then((error) => {
+      msg("Service worker registration failed", messagebox, debug);
+    });
+  
+  } else {
+    msg("Service Workers are not supported in your browser. Your navigator is: " + navigator.userAgent, messagebox, debug);
+  }
+}
